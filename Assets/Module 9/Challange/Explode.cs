b505d8f057e50eb574ff4d37d572fa3259254646
atauahdj <antonnn.518.bk@gmail.com>
2026-04-29 14:46:00 +0300

    Challange 9 Module

--- Assets/Module 9/Challange/Explode.cs
+++ Assets/Module 9/Challange/Explode.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Explode : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    public KeyCode KeyInput;
+    private bool isGrounded = false;
+    public float X;
+    public float Z;
+    private void Start()
     {
-        if(collision.gameObject.name == "Terrain")
+        gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyInput))
         {
-            ExplodeFunc();
+            gameObject.GetComponent<Rigidbody>().isKinematic = false;
+        }
+        Debug.Log(isGrounded);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Terrain"))
+        {
+            isGrounded = true;
         }
     }
-    void ExplodeFunc()
+    void OnTriggerEnter(Collider other)
     {
-
+        if (other.gameObject.CompareTag("Target"))
+        {
+            X = Random.Range(-10f, 10f);
+            Z = X;
+            if (isGrounded)
+            {
+                other.gameObject.GetComponent<Rigidbody>().AddForce(X, 30f, Z, ForceMode.Impulse);
+                isGrounded = false;
+            }
+        }
     }
 }
